0b8da28dfa558b296067c6c3cf442f0279d10042
Tahsin Ahmad <tahsinahmad2004@gmail.com>
2025-07-31 15:09:04 +0600

    first module code

---
+++ Week-1/First_Code.C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // Variable
+    int a = 10;
+    int b = 10;
+    float c = 10.90;
+    char word = 'a';
+
+    // Let's try to see output
+    // printf("%d", a);
+    //   printf("%c", word);
+
+    // change value
+    int num1 = 10;
+    int num2;
+
+    num2 = num1;
+
+    //   printf("%d", num2);
+
+    // multiple variable with values
+
+    int x = 10, y = 20, z = 30;
+
+    printf("%d %d %d", y, x, z);
+
+    return 0;
+}
