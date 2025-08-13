b218b86f76a9a429896963a232ef7bf4cf11098d
Tahsin Ahmad <tahsinahmad2004@gmail.com>
2025-08-13 16:33:24 +0600

    add 2 file

---
+++ loop.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+// While Loop 
+int main(){
+    int x = 100; 
+    
+    while (x >= 0){
+        printf("Sorry\n"); 
+        x-- ; 
+    }
+}
+
+// Do while Loop 
+
+int main(){
+      int x = 100; 
+    
+    do{
+        printf("Sorry\n"); 
+        x-- ; 
+    }
+    while(x >= 0); 
+    
+}
+
+// For Loop
+
+int main(){
+    
+    int x ; 
+    for(x = 1; x <= 100; x++){
+        printf("I Love You\n"); 
+    }
+}
+
+
+/*
+Assignment: 
+1. let's create a program that counts to 100 by tens 
+2. Print 'LoopNest' 200 times
+3. We create a program that only print even numbers between 0 and 10
+4. We create a program that only print odd numbers between 0 and 10 
+
+*/
---
+++ switch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main(){
+    // switch
+    
+    int x = 4;
+    
+    switch (x){
+        case 1:
+        printf("Saturday");
+        break ; 
+        
+        case 2: 
+        printf("Sunday");
+        break; 
+        
+        case 3: 
+        printf("Monday"); 
+        break; 
+        
+        default:
+            printf("Nothing"); 
+        
+    }
+}
